6c96dbf42048f55e1dd1b9e598ffaf1e21ddc43e
Isis Dumas <isis.dumas000@gmail.com>
2024-06-04 13:23:42 -0700

    Moved side_right content to main.js to be used on multiple pages

--- main.js
+++ main.js
@@ -2,13 +2,58 @@ function load_divs() {
     let side_left = document.getElementById("side_left");
 
     let child = document.createElement('div');
-
     child.innerHTML = 
         `<div class="window_hide">
             <h2> <u class="text_shadow">Left Side</u> </h2>
         </div>`
     
     side_left.appendChild(child);
+
+    let side_right = document.getElementById("side_right");
+
+    child = document.createElement('div');
+    child.innerHTML =
+        `<div class="window_hide">
+        <h2><u class="text_shadow">Fun Facts:</u></h2> 
+      </div>
+
+      <div class="window_hide side_content">
+        <p>Favorite Color: </p>
+        <p> Red </p>
+        <img class="img-fluid" src="img/red.jpg" alt="Red">
+      </div>
+
+      <div class="window_hide side_content">
+        <p>Favorite Food: </p>
+        <p>Mac N Cheese </p>
+        <img class="img-fluid" src="img/mac_n_cheese.jpg" alt="Mac N Chesse">
+      </div>
+
+      <div class="window_hide side_content">
+        <p>Favorite Candy: </p>
+        <p>Mac N Cheese </p>
+        <img class="img-fluid" src="img/green_rings.jpg" alt="Green Apple Rings">
+      </div>
+
+      <div class="window_hide side_content">
+        <p>Favorite Drink: </p>
+        <p>Boba </p>
+        <img class="img-fluid" src="img/boba.jpg" alt="Boba">
+      </div>
+
+      <div class="window_hide side_content">
+        <p>Favorite Cartoon: </p>
+        <p>Adventure Time </p>
+        <img class="img-fluid" src="img/cartoon.jpg" alt="Adventure Time">
+      </div>
+
+      <div class="window_hide side_content">
+        <p>Favorite Anime: </p>
+        <p> Nana </p>
+        <img class="img-fluid" src="img/nana.jpg" alt="Nana">
+      </div>`
+
+      side_right.appendChild(child);
 }
 
 function show_relative_coursework() {
